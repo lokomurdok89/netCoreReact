953e1b38c9801a07a17e45cc97b2e14e8aacde13
Daniel Toledo <Daniel.Toledo@belltech.la>
2023-02-06 12:42:47 -0300

    se agregan los cambios

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -16,6 +16,7 @@ namespace API.Controllers
          [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities(){
             return await _context.Activities.ToListAsync();
+            
 
         }    
         [HttpGet("{id}")]  
